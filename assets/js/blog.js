fda973454d3b859ef778c1af062f1320ef33362d
CodeChef <joetomsim@gmail.com>
2024-04-05 21:54:19 -0500

    added function to the modeButton and the Submit button

---
+++ assets/js/blog.js
@@ -0,0 +1,12 @@
+const modeButton = document.querySelector('#modeButton');
+console.log()
+
+
+// light/dark function
+function modeSwitch()  {
+  console.log('Click!')
+  let element = document.body;
+  element.classList.toggle("darkMode");
+}
+
+// modeButton.addEventListener('click' modeSwitch());
