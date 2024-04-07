d6d8b4da15fae97397aa9bfb81df91f470895c87
CodeChef <joetomsim@gmail.com>
2024-04-06 22:42:34 -0500

    worked on functions in js

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,1 +1,11 @@
 //dynamically generated blogs stuff
+
+
+//retrieves user input from localStorage
+
+function readPost() {
+  const blogPost = JSON.parse(localStorage.getItem('formContent'));
+  if (blogPost !== null) {
+//details
+  }
+}
